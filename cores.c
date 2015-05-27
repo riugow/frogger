656f8f69e59b668c6e7b1c784689d6813f94011d
riugow <hugo.s.costa@gmail.com>
2015-05-27 17:08:46 -0300

    Arquivo desnecessário para o projeto

--- cores.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("\033[10;30f\e[31mHello\e[32m, \e[34mBlue \e[32mWorld\e[0m.\n");
-    system(g"color f1");
-    printf("\n\n\t\t teste do comando system-color f1");
-    return 0;
-}
